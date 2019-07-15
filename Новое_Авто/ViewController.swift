d1eabe259ac805f1011812cca04f567f0e47b389
Sergei Kazakov <skazakovru@gmail.com>
2019-07-15 11:50:48 +0300

    Initial Commit

---
+++ Новое_Авто/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Новое_Авто
+//
+//  Created by Sergei Kazakov on 7/15/19.
+//  Copyright © 2019 Sergei Kazakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
