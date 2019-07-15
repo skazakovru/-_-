724a62c153ebbf02866c5a435d4fc26cb64b274b
Sergei Kazakov <skazakovru@gmail.com>
2019-07-15 18:51:34 +0300

    Complete with navigation bar colored

--- Новое_Авто/TableViewController.swift
+++ Новое_Авто/TableViewController.swift
@@ -11,22 +11,22 @@ import UIKit
 
 class TableViewController: UITableViewController {
     
-    var names = [String]()
+    var itemArray = [String]()
     var identities = [String]()
     
     override func viewDidLoad() {
         
-        names = ["Документы","Маркировка","Комплектация","Дополнительное оборудование","Диски и шины","Внешний осмотр","Осмотр кузова и его элементов","Уровень технических жидкостей","Осмотр салона","Проверка работоспособности основных узлов и агрегатов автомобиля"]
+        itemArray = ["Документы","Маркировка","Комплектация","Дополнительное оборудование","Диски и шины","Внешний осмотр","Осмотр кузова и его элементов","Уровень технических жидкостей","Осмотр салона","Проверка работоспособности основных узлов и агрегатов автомобиля"]
         identities = ["A","B","C","D","E","F","G","H","I","J"]
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return itemArray.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
         
-        cell?.textLabel?.text = names[indexPath.row]
+        cell?.textLabel?.text = itemArray[indexPath.row]
         return cell!
     }
     
